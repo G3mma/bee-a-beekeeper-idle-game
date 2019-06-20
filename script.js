71856b0856f9444d5cb6b23aee374f9860f67f81
G3mma <50620318+G3mma@users.noreply.github.com>
2019-06-20 14:43:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-    
-    x += dx;
-    y += dy;
-}
-
-setInterval(draw, 10);
+var gen1amount = 0, gen1price = 15, gen1baseprice = 15, credit = 0, cps = 0;
+    function update(){
+        click.onclick = function() {credit = credit + 1};
+        generator1.onclick = function() {
+        if (credit >= gen1price) {
+            credit = credit - gen1price;
+            gen1amount = gen1amount + 1;
+            cps = cps + 1;
+            gen1price = gen1baseprice * (Math.pow(1.15, gen1amount));
+            };
+        };
+        credit = credit + (cps/60);
+    };
+    function draw(){
+        creditPerSecond.value = cps.toFixed(0);
+        creds.value = credit.toFixed(0);
+        generator1.value = gen1price.toFixed(0);
+        generator1amount.value = gen1amount.toFixed(0);
+    };
+    var mainloop = function() {update(), draw()}; 
+    setInterval(mainloop, 16);
